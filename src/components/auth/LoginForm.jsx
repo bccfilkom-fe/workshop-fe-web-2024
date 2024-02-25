774416ce94281887c5c70752b2b7f63ae8c24ab6
tasyaswantara <tasyaswantara@gmail.com>
2024-02-25 15:32:34 +0700

    style: format document prettier

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -3,58 +3,61 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Input from "../ui/Input";
-import  Button from "../ui/Button";
+import Button from "../ui/Button";
 
 const LoginForm = () => {
-    const navigate =useNavigate();
-    const [form, setForm]= useState({
-        email: "",
-        password: ""
-    })
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handlesubmit =(e)=>{
-        e.preventDefault();
-        console.log(form);
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
 
-        setTimeout(()=>{
-            navigate("/")
-        },1000)
-    }
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  };
 
-    return ( 
-        <div className="p-5 md:p-10 rounded-3xl bg-white w-[calc(100vw-40px)] max-w-[500px] flex flex-col gap-2 h-fit">
-            <h3 className=" text-xl md:-2xl font-semibold text-center md:text-left ">Login Yukkkk!</h3>
+  return (
+    <div className="p-5 md:p-10 rounded-3xl bg-white w-[calc(100vw-40px)] max-w-[500px] flex flex-col gap-2 h-fit">
+      <h3 className=" text-xl md:-2xl font-semibold text-center md:text-left ">
+        Login Yukkkk!
+      </h3>
 
-            <form className=" flex flex-col gap-4">
-            <Input
-            type="email"
-            label={"Email"}
-            name={"email"}
-            onChange={(e)=> setForm({...form,email: e.target.value})}
-            required={true}
-            />
-            <Input
-            type="password"
-            label={"Password"}
-            name={"password"}
-            onChange={(e)=> setForm({...form, password: e.target.value})}
-            required={true}
-            />
+      <form className=" flex flex-col gap-4">
+        <Input
+          type="email"
+          label={"Email"}
+          name={"email"}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          required={true}
+        />
+        <Input
+          type="password"
+          label={"Password"}
+          name={"password"}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          required={true}
+        />
 
-            <Button
-            type={"submit"}
-            variation={"secondary"}
-            className={" w-full mt-2"}
-            onClick={handlesubmit}
-            >
-                Login
-            </Button>
+        <Button
+          type={"submit"}
+          variation={"secondary"}
+          className={" w-full mt-2"}
+          onClick={handlesubmit}
+        >
+          Login
+        </Button>
 
-            <p className=" w-full text-center text-sm md:text-base">Not Registered? <a href="/signup">Create an account</a></p>
-            </form>
+        <p className=" w-full text-center text-sm md:text-base">
+          Not Registered? <a href="/signup">Create an account</a>
+        </p>
+      </form>
+    </div>
+  );
+};
 
-        </div>
-     );
-}
- 
 export default LoginForm;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import MainLayout from "../components/layout/MainLayout";
 import LoginForm from "../components/auth/LoginForm";
 const LoginPage = () => {
-    return ( 
-        <MainLayout >
-            <main className=" flex flex-col items-center w-screen">
-            <LoginForm/>
-            </main>
-           
-        </MainLayout>
-     );
-}
- 
+  return (
+    <MainLayout>
+      <main className=" flex flex-col items-center w-screen">
+        <LoginForm />
+      </main>
+    </MainLayout>
+  );
+};
+
 export default LoginPage;
